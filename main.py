416b494310e4c5a1631eb52564309ed3709cf9c9
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-09 01:23:28 +0530

    Updated for qna model

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from time import perf_counter
 from flask import Flask, request, jsonify
 from transformers import GPTJForCausalLM, AutoTokenizer
 
+logger = logging.getLogger(__name__)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+logger.info(f"Using device {device}")
+print(f"Using device {device}")
 [print(torch.cuda.get_device_properties(i)) for i in range(torch.cuda.device_count())] if torch.cuda.is_available() else print('cpu')
 
 model_name_or_path = "EleutherAI/gpt-j-6b"
 model_name_or_path = "./model/"
 tokenizer_name_or_path = "EleutherAI/gpt-j-6b"
-logger = logging.getLogger(__name__)
 
 s = perf_counter()
 model = GPTJForCausalLM.from_pretrained(model_name_or_path).to(device)
