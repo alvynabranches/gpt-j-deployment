1409d0d644e732493da77ac7c3bca41572c28d4e
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-09 00:25:42 +0530

    Updated for qna model

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 mkdir model
-# gsutil -m cp \
-#   "gs://gptjax_model_weights/hf_weights_reddit/config.json" \
-#   "gs://gptjax_model_weights/hf_weights_reddit/pytorch_model.bin" \
-#   model/.
-
-gsutil -m cp gptjax_model_weights/qna_weights_slim_f16/hf_weights/config.json model/.
-gsutil -m cp gptjax_model_weights/qna_weights_slim_f16/hf_weights/pytorch_model.bin model/.
+gsutil -m cp \
+  "gs://gptjax_model_weights/qna_weights_slim_f16/hf_weights/config.json" \
+  "gs://gptjax_model_weights/qna_weights_slim_f16/hf_weights/pytorch_model.bin" \
+  model/.
 
 sudo nvidia-docker image build -t jaxdeploy .
 sudo nvidia-docker container run --name jd -d --rm -p 5000:5000 jaxdeploy
