913db8677d586ebc8eef13438a2dcf6aff24226a
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-09 01:32:02 +0530

    Updated for qna model

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #   "gs://gptjax_model_weights/qna_weights_slim_f16/hf_weights/pytorch_model.bin" \
 #   model/.
 
-nvidia-docker image build -t alvynabranches/jaxdeploy .
+# nvidia-docker image build -t alvynabranches/jaxdeploy .
 nvidia-docker container run --gpus=all --name jd -d -p 5000:5000 alvynabranches/jaxdeploy
 
 watch -n 1 nvidia-docker container logs jd
