1542f0c3705ea373e2db516b7465292a0bba311e
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-09 22:31:46 +0530

    Updated for ltm model

--- ltm.sh
+++ ltm.sh
@@ -2,7 +2,7 @@ mkdir model
 gsutil -m cp \
   "gs://gptjax_model_weights/msc_ltm_weights_slim_f16/hf_weights/config.json" \
   "gs://gptjax_model_weights/msc_ltm_weights_slim_f16/hf_weights/pytorch_model.bin" \
-  .
+  model/.
 
 sudo docker image build -t alvynabranches/jax:ltm .
 # sudo docker container run --name ltm -d -p 5000:5000 alvynabranches/jax:ltm
