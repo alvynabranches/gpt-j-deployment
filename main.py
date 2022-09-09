3df0bd105f8a6e405ce576579bccf116e0f6f31b
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-09 19:56:09 +0530

    Updated for qna model

--- main.py
+++ main.py
@@ -92,4 +92,5 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("HOST", "0.0.0.0"), 
         port=os.environ.get("PORT", 5000), 
-        debug=os.environ.get("DEBUG", True))
+        debug=os.environ.get("DEBUG", True)
+    )
