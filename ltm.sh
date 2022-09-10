1e8475a8bfeb4c59f1401e03a6476882ec451dbd
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-10 14:56:47 +0530

    Updated for all models

--- ltm.sh
+++ ltm.sh
@@ -1,8 +1,18 @@
 mkdir ltm_model
-gsutil -m cp \
-  "gs://gptjax_model_weights/msc_ltm_weights_slim_f16/hf_weights/config.json" \
-  "gs://gptjax_model_weights/msc_ltm_weights_slim_f16/hf_weights/pytorch_model.bin" \
-  ltm_model/.
+
+if [ -f "ltm_model/config.json" ]; then
+  echo "config.json exists"
+else
+  echo "config.json does not exist"
+  gsutil -m cp "gs://gptjax_model_weights/msc_ltm_weights_slim_f16/hf_weights/config.json" ltm_model/.
+fi
+
+if [ -f "ltm_model/pytorch_model.bin" ]; then
+  echo "pytorch_model.bin exists"
+else
+  echo "pytorch_model.bin does not exist"
+  gsutil -m cp "gs://gptjax_model_weights/msc_ltm_weights_slim_f16/hf_weights/pytorch_model.bin" ltm_model/.
+fi
 
 sudo docker image build -t alvynabranches/jax:ltm -f ltm.Dockerfile .
 sudo docker push alvynabranches/jax:ltm
--- qna.sh
+++ qna.sh
@@ -1,8 +1,18 @@
 mkdir qna_model
-gsutil -m cp \
-  "gs://gptjax_model_weights/qna_weights_slim_f16/hf_weights/config.json" \
-  "gs://gptjax_model_weights/qna_weights_slim_f16/hf_weights/pytorch_model.bin" \
-  qna_model/.
+
+if [ -f "ltm_model/config.json" ]; then
+  echo "config.json exists"
+else
+  echo "config.json does not exist"
+  gsutil -m cp "gs://gptjax_model_weights/qna_weights_slim_f16/hf_weights/config.json" qna_model/.
+fi
+
+if [ -f "ltm_model/pytorch_model.bin" ]; then
+  echo "pytorch_model.bin exists"
+else
+  echo "pytorch_model.bin does not exist"
+  gsutil -m cp "gs://gptjax_model_weights/qna_weights_slim_f16/hf_weights/pytorch_model.bin" qna_model/.
+fi
 
 sudo docker image build -t alvynabranches/jax:qna -f qna.Dockerfile .
 sudo docker push alvynabranches/jax:qna
