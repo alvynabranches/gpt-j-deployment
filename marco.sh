05b9a5398aa3e516d8c5c35e47fbaff11b5080db
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-12 02:11:04 +0530

    Updated for marco model

--- marco.sh
+++ marco.sh
@@ -4,14 +4,14 @@ if [ -f "marco_model/config.json" ]; then
   echo "config.json exists"
 else
   echo "config.json does not exist"
-  gsutil -m cp "gptjax_model_weights/ms_marco_weights_slim_f16/hf_weights/config.json" marco_model/.
+  gsutil -m cp "gs://gptjax_model_weights/ms_marco_weights_slim_f16/hf_weights/config.json" marco_model/.
 fi
 
 if [ -f "marco_model/pytorch_model.bin" ]; then
   echo "pytorch_model.bin exists"
 else
   echo "pytorch_model.bin does not exist"
-  gsutil -m cp "gptjax_model_weights/ms_marco_weights_slim_f16/hf_weights/pytorch_model.bin" marco_model/.
+  gsutil -m cp "gs://gptjax_model_weights/ms_marco_weights_slim_f16/hf_weights/pytorch_model.bin" marco_model/.
 fi
 
 sudo docker image build -t alvynabranches/jax:marco -f marco.Dockerfile .
