70cac900879a4ec052fe3aa2ff25b07ad978bfad
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-12 01:32:12 +0530

    Updated for qna model

--- qna.sh
+++ qna.sh
@@ -1,13 +1,13 @@
 mkdir qna_model
 
-if [ -f "ltm_model/config.json" ]; then
+if [ -f "qna_model/config.json" ]; then
   echo "config.json exists"
 else
   echo "config.json does not exist"
   gsutil -m cp "gs://gptjax_model_weights/qna_weights_slim_f16/hf_weights/config.json" qna_model/.
 fi
 
-if [ -f "ltm_model/pytorch_model.bin" ]; then
+if [ -f "qna_model/pytorch_model.bin" ]; then
   echo "pytorch_model.bin exists"
 else
   echo "pytorch_model.bin does not exist"
