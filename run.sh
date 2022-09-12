007cda1ef2c629b58bcf31434316c5bfdaddae56
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-12 11:35:29 +0530

    Updated for all models

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 git pull origin main
 sudo docker login
+
+source base.sh
+# source qna_base.sh
+# source ltm_base.sh
+# source empathy_base.sh
+# source marco.sh
+
 source qna.sh
 source ltm.sh
 source empathy.sh
