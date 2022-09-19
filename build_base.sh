ad6756158216641a7f8c5afafb6d7b567b5442a0
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-19 16:28:41 +0530

    Updated all models

--- build_base.sh
+++ build_base.sh
@@ -4,9 +4,13 @@ git pull origin main
 source base.sh
 
 source empathy_base.sh
+rm -rf empathy_model
 source ltm_base.sh
+rm -rf ltm_model
 source marco_base.sh
+rm -rf marco_model
 source qna_base.sh
+rm -rf qna_model
 
 source empathy.sh
 source ltm.sh
