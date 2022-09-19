e6ee19d40aca3c8110bde640ecc644727f328be9
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-19 18:51:08 +0530

    Update for all models

--- qna_infer.sh
+++ qna_infer.sh
@@ -2,3 +2,4 @@ sudo gcloud auth configure-docker
 
 sudo docker pull gcr.io/gpt-j-and-gpt-neox20b/qna:latest
 sudo docker container run --name infer --rm -d -p 5000:5000 gcr.io/gpt-j-and-gpt-neox20b/qna:latest
+sudo docker container logs infer
