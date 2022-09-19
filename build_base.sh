70ba53a0aa226710200ae2acbf9db020c35d0e7d
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-19 15:07:13 +0530

    Updated all models

--- build_base.sh
+++ build_base.sh
@@ -5,12 +5,16 @@ git pull origin main
 
 # source empathy_base.sh
 # rm -rf empathy_model/
+sudo docker image rm empathy:base
 source ltm_base.sh
 rm -rf ltm_model/
+sudo docker image rm ltm:base
 source marco_base.sh
 rm -rf marco_model/
+sudo docker image rm marco:base
 source qna_base.sh
 rm -rf qna_model/
+sudo docker image rm qna:base
 
 source empathy.sh
 source ltm.sh
