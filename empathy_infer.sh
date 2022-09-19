7bf19fae19ae25e39a0dbe266bc246e1315acf68
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-19 20:48:35 +0530

    Update for all models

--- empathy_infer.sh
+++ empathy_infer.sh
@@ -3,3 +3,4 @@ sudo gcloud auth configure-docker
 sudo docker pull gcr.io/gpt-j-and-gpt-neox20b/empathy:latest
 sudo docker container run --name infer --rm -d -p 5000:5000 gcr.io/gpt-j-and-gpt-neox20b/empathy:latest
 sudo docker container logs infer
+watch -n 1 sudo docker container logs infer
