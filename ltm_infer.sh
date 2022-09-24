b7c19c4a354e931d6a1888618d43b1ab158e29e1
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-24 13:29:14 +0530

    Updated all models

--- ltm_infer.sh
+++ ltm_infer.sh
@@ -1,6 +1,6 @@
 sudo gcloud auth configure-docker
 
 sudo docker pull gcr.io/gpt-j-and-gpt-neox20b/ltm:latest
-sudo docker container run --name infer --restart always --rm -d -p 5000:5000 gcr.io/gpt-j-and-gpt-neox20b/ltm:latest
+sudo docker container run --name infer --restart always -d -p 5000:5000 gcr.io/gpt-j-and-gpt-neox20b/ltm:latest
 sudo docker container logs infer
 watch -n 1 sudo docker container logs infer
