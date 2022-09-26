b23ec93e5871cc6fa25cc0c45f053142b8acd4d0
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-26 19:15:00 +0530

    Updated all models

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt install git curl docker docker.io htop bpytop python3-pip -y
 git clone https://github.com/alvynabranches/gpt-j-deployment
 cd gpt-j-deployment
-python3 -m pip install --upgrade pip setuptools
-pip install -r requirements.txt --no-warn-script-location
+# python3 -m pip install --upgrade pip setuptools
+# pip install -r requirements.txt --no-warn-script-location
