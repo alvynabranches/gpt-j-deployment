c6f8c6d88b1def0c4cb01ac8597353562236a7ac
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-28 17:15:17 +0530

    Updated nginx

--- nginx.sh
+++ nginx.sh
@@ -1,1 +1,1 @@
-sudo docker container run --restart=always -v ./nginx.conf:/etc/nginx/nginx.conf -p 80:80 --name server -d nginx
+sudo docker container run --restart=always -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf -p 80:80 --name server -d nginx
