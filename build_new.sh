54d71e4032718b36bc6c83a8f8758558eaa6a250
Alvyn Abranches <alvynabranches@gmail.com>
2022-09-28 15:17:19 +0530

    Updated wow model

--- build_new.sh
+++ build_new.sh
@@ -20,23 +20,29 @@
 # echo "Done with empathy_latest.sh"
 # sudo docker rmi gcr.io/gpt-j-and-gpt-neox20b/empathy:base gcr.io/gpt-j-and-gpt-neox20b/empathy:latest
 
-source iag_base.sh
-echo "Done with iag_base.sh"
+# source iag_base.sh
+# echo "Done with iag_base.sh"
 
-source iag_latest.sh
-echo "Done with iag_latest.sh"
-sudo docker rmi gcr.io/gpt-j-and-gpt-neox20b/iag:base gcr.io/gpt-j-and-gpt-neox20b/iag:latest
+# source iag_latest.sh
+# echo "Done with iag_latest.sh"
+# sudo docker rmi gcr.io/gpt-j-and-gpt-neox20b/iag:base gcr.io/gpt-j-and-gpt-neox20b/iag:latest
 
-source marco_base.sh
-echo "Done with marco_base.sh"
+# source marco_base.sh
+# echo "Done with marco_base.sh"
 
-source marco_latest.sh
-echo "Done with marco_latest.sh"
-sudo docker rmi gcr.io/gpt-j-and-gpt-neox20b/marco:base gcr.io/gpt-j-and-gpt-neox20b/marco:latest
+# source marco_latest.sh
+# echo "Done with marco_latest.sh"
+# sudo docker rmi gcr.io/gpt-j-and-gpt-neox20b/marco:base gcr.io/gpt-j-and-gpt-neox20b/marco:latest
 
-source qna_base.sh
-echo "Done with qna_base.sh"
+# source qna_base.sh
+# echo "Done with qna_base.sh"
 
-source qna_latest.sh
-echo "Done with qna_latest.sh"
-sudo docker rmi gcr.io/gpt-j-and-gpt-neox20b/qna:base gcr.io/gpt-j-and-gpt-neox20b/qna:latest
+# source qna_latest.sh
+# echo "Done with qna_latest.sh"
+# sudo docker rmi gcr.io/gpt-j-and-gpt-neox20b/qna:base gcr.io/gpt-j-and-gpt-neox20b/qna:latest
+
+source wow_base.sh
+echo "Done with wow_base.sh"
+
+source wow_latest.sh
+echo "Done with wow_latest.sh"
