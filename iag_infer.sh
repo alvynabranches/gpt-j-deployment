259dfa2736777c5f6d647e2d76c4bc175b78288e
Alvyn Abranches <alvynabranches@gmail.com>
2022-10-10 14:41:41 +0530

    Updated iag model

--- iag_infer.sh
+++ iag_infer.sh
@@ -1,6 +1,20 @@
-sudo gcloud auth configure-docker
+directory="iag_model"
+bucket="gptjax_model_weights"
+folder="reddit_IAG_slim_f16"
+
+if [ ! -d "${directory}" ]; then
+  mkdir ${directory}
+fi
+
+if [ ! -f "${directory}/config.json" ]; then
+  gsutil -m cp "gs://${bucket}/${folder}/hf_weights/config.json" ${directory}/.
+fi
+
+if [ ! -f "${directory}/pytorch_model.bin" ]; then
+  gsutil -m cp "gs://${bucket}/${folder}/hf_weights/pytorch_model.bin" ${directory}/.
+fi
 
 sudo docker pull gcr.io/gpt-j-and-gpt-neox20b/iag:latest
-sudo docker container run --name infer --restart always -d -p 5000:5000 gcr.io/gpt-j-and-gpt-neox20b/iag:latest
+sudo docker container run --name infer --restart always -d -p 5000:5000  -v $(pwd)/iag_model/:/app/model/gcr.io/gpt-j-and-gpt-neox20b/iag:latest
 sudo docker container logs --tail 10 infer
 watch -n 1 sudo docker container logs --tail 10 infer
