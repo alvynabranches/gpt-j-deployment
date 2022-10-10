f4167f61097030b34f861acf5a74deeefddf9e1f
Alvyn Abranches <alvynabranches@gmail.com>
2022-10-10 14:44:27 +0530

    Updated lfqa model

--- lfqa.Dockerfile
+++ lfqa.Dockerfile
@@ -3,6 +3,7 @@ FROM gcr.io/gpt-j-and-gpt-neox20b/lfqa:base
 ENV DEBIAN_FRONTEND=noninteractive
 
 WORKDIR /app
+VOLUME /app/model/
 
 COPY requirements.txt requirements.txt
 RUN pip3 install --upgrade -r requirements.txt
@@ -12,4 +13,3 @@ ENV MODEL_NAME=lfqa
 EXPOSE 5000
 
 CMD [ "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "5000" ]
-# CMD [ "python3", "main.py" ]
--- lfqa_infer.sh
+++ lfqa_infer.sh
@@ -1,6 +1,19 @@
-sudo gcloud auth configure-docker
+directory="lfqa_model"
+bucket="gptjax_model_weights"
+folder="reddit_lfqa_slim_f16"
+
+if [ ! -d "${directory}" ]; then
+  mkdir ${directory}
+fi
+
+if [ ! -f "${directory}/config.json" ]; then
+  gsutil -m cp "gs://${bucket}/${folder}/hf_weights/config.json" ${directory}/.
+fi
+
+if [ ! -f "${directory}/pytorch_model.bin" ]; then
+  gsutil -m cp "gs://${bucket}/${folder}/hf_weights/pytorch_model.bin" ${directory}/.
+fi
 
 sudo docker pull gcr.io/gpt-j-and-gpt-neox20b/lfqa:latest
-sudo docker container run --name infer --restart always -d -p 5000:5000 gcr.io/gpt-j-and-gpt-neox20b/lfqa:latest
-sudo docker container logs --tail 10 infer
-watch -n 1 sudo docker container logs --tail 10 infer
+sudo docker container run --name infer --restart always -d -p 5000:5000 -v $(pwd)/lfqa_model/:/app/model/ gcr.io/gpt-j-and-gpt-neox20b/lfqa:latest
+watch -n 1 sudo docker container logs --tail 25 infer
