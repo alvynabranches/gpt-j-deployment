55fec7bfb79b97acd2714e77e8b8416b3e943988
Alvyn Abranches <alvynabranches@gmail.com>
2022-10-10 18:13:26 +0530

    Updated all models

--- build_latest.sh
+++ build_latest.sh
@@ -1,3 +1,10 @@
+git pull origin main
+
+sudo gcloud auth configure-docker
+
+source base.sh
+echo -e "Completed base.sh\n\n\n"
+
 source empathy_latest.sh
 echo -e "Completed empathy_latest.sh\n\n\n"
 
@@ -9,9 +16,6 @@ echo -e "Completed ltm_latest.sh\n\n\n"
 
 source marco_latest.sh
 echo -e "Completed marco_latest.sh\n\n\n"
-
-source marco_old_latest.sh
-echo -e "Completed marco_old_latest.sh\n\n\n"
 
 source qna_latest.sh
 echo -e "Completed qna_latest.sh\n\n\n"
